7aeec06a22065503169fa0275cd2b90fceaf7add
Roman Dudko <romandudko@touchsoft.by>
2020-08-03 13:46:43 +0300

    added unit tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from handlers.routes import configure_routes
 import random
 import string
+from flask import Flask
+from handlers.routes import configure_routes
 
 
 def test_version_route():
@@ -41,7 +41,7 @@ def test_bird_route_post_wrong_data_type():
     url = '/birds'
 
     mock_request_data = {
-        "body_length": "1000",
+        "body_length": "WRONG_DATA_TYPE",
         "color": "red & white",
         "name": "test_bird",
         "species": "pigeon",
@@ -83,15 +83,77 @@ def test_bird_route_post_duplicate_entity():
         "wingspan": 1000
     }
 
-    response = client.post(url, json=mock_request_data)
+    response_init = client.post(url, json=mock_request_data)
     response = client.post(url, json=mock_request_data)
     assert response.status_code == 400
 
 
-# test_bird_route_get_all
-# get sorted asc/desc
-# get offset/limit
-# combined request
+def test_bird_route_get_basic():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/birds'
 
-# empty paeams
-# incorrect params
+    response = client.get(url)
+    assert response.status_code == 200
+    assert len(response.get_data()) > 0
+
+
+def test_bird_route_get_sorted():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/birds'
+
+    response = client.get(url, query_string={'attribute': 'name', 'order': 'asc'})
+    assert response.status_code == 200
+    attr_list = [item['name'] for item in response.get_json()]
+    assert attr_list == sorted(attr_list)
+
+
+def test_bird_route_get_limit():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/birds'
+
+    response = client.get(url, query_string={'limit': 2})
+    assert response.status_code == 200
+    assert len(response.get_json()) == 2
+
+
+def test_bird_route_get_offset():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/birds'
+
+    response_full = client.get(url)
+    data1 = response_full.get_json()
+    res1 = data1[5]
+    response = client.get(url, query_string={'offset': 5})
+    data2 = response.get_json()
+    res2 = data2[0]
+    assert response.status_code == 200
+    assert res1 == res2
+
+
+def test_bird_route_get_empty_params():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/birds'
+
+    response = client.get(url, query_string={'limit': None})
+    assert response.status_code == 200
+    assert len(response.get_data()) > 0
+
+
+def test_bird_route_get_wrong_data_type():
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    url = '/birds'
+
+    response = client.get(url, query_string={'limit': "Hello"})
+    assert response.status_code == 500
